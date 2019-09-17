67179558b574fd111c03cac70bb821c37ced1337
mapama247 <mpamies247@gmail.com>
2019-09-17 17:25:28 +0200

    added documentation for euclidean function

--- lab3/R/euclidean.R
+++ lab3/R/euclidean.R
@@ -1,11 +1,19 @@
-# 3.1.1 EUCLIDEAN ALGORITHM
+#' @title Euclidean Algorithm
+#' 
+#' @description \code{euclidean} finds the greatest common divisor of two integers.
+#' 
+#' @param a A number.
+#' @param b A number.
+#' @return Greatest common divisor between \code{a} and \code{b}
+#' @seealso \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Wikipedia}
+#' @aliases eucl euclidean_algorithm
+#' @family \code{\link{dijkstra}}
+#' @examples
+#' euclidean(123612,13892347912)
+#' euclidean(100,1000)
 
-# function that finds thes greatest common divisor of two given integers
-# pseudocode: https://en.wikipedia.org/wiki/Euclidean_algorithm
 euclidean <- function(a,b){
-	# Assert that the arguments are numeric scalars or integers
 	stopifnot( !is.na(as.numeric(a)) , !is.na(as.numeric(a)) )
-	
 	while(b!=0){
 		aux <- b
 		b <- a %% b
@@ -13,6 +21,3 @@ euclidean <- function(a,b){
 	}
 	return(a)
 }
-
-euclidean(123612, 13892347912) # 4
-euclidean(100, 1000) # 100
