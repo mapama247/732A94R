95394ac9729b3f175a75b9813f4b89788b44a126
mapama247 <mpamies247@gmail.com>
2019-09-17 17:41:08 +0200

    added wiki_graph data with documentation

---
+++ lab3/R/data.R
@@ -0,0 +1,11 @@
+#' Example Graph
+#' 
+#' A dataset containing blablabla
+#' 
+#' @format A dataframe with 18 rows and 3 variables:
+#' \describe{
+#' \item{v1}{starting node, no unit}
+#' \item{v2}{ending node, no unit}
+#' \item{w}{weight of the link, no unit}
+#' }
+"wiki_graph"
