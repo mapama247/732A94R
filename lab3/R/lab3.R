df79862a937941b3eab88c1612c15014c7dc9788
mapama247 <mpamies247@gmail.com>
2019-09-18 22:28:47 +0200

    moved yaml file

--- lab3/R/lab3.R
+++ lab3/R/lab3.R
@@ -4,10 +4,10 @@
 #' euclidean(a,b) and dijkstra(g,i)
 #' 
 #' @section lab3 functions:
-#' The function euclidean(a,b) finds the greatest common divisor of two numbers.
-#' The function dijkstra(g,i) finds the shortest path from an initial node to every other node in a graph.
+#' The function euclidean(a,b) finds the greatest common divisor between the numbers a and b.
+#' The function dijkstra(graph,initial_node) finds the shortest path from an initial_node to every other node in graph.
 #' 
 #' @docType package
 #' @name lab3
-NULL
-#> NULL
+#' @export
+"_PACKAGE"
