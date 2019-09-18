47cc98b7e6ef8599b5beca48b72d87d5afdcf8d5
mapama247 <mpamies247@gmail.com>
2019-09-19 00:22:18 +0200

    final final changes

--- lab3/R/lab3.R
+++ lab3/R/lab3.R
@@ -1,10 +1,13 @@
-#' lab3: A package to apply both the Euclidean and Dijkstra algorithms.
+#' lab3
+#' 
+#' A package to apply both the Euclidean and Dijkstra algorithms.
 #' 
 #' The package provides 2 different functions:
 #' euclidean(a,b) and dijkstra(g,i)
 #' 
-#' @section lab3 functions:
-#' The function euclidean(a,b) finds the greatest common divisor between the numbers a and b.
+#' @section Functions:
+#' The function euclidean(a,b) finds the greatest common divisor between a and b.
+#' 
 #' The function dijkstra(graph,initial_node) finds the shortest path from an initial_node to every other node in graph.
 #' 
 #' @docType package
